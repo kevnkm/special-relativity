9887019709c846994cc10271384ab7b9c9276cb8
Kevin Kim <kk946@cornell.edu>
2025-07-22 17:59:21 +0900

    Update Event_0.cs

--- SpecialRelativity/Assets/Scripts/Event_0.cs
+++ SpecialRelativity/Assets/Scripts/Event_0.cs
@@ -10,7 +10,7 @@ public class Event_0 : MonoBehaviour
 
     private IEnumerator Walk()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(3f);
         DialogueManager.Instance.EinsteinAnimator.SetTrigger("Walk Start");
         yield return new WaitForSeconds(3f);
         DialogueManager.Instance.EinsteinAnimator.SetTrigger("Walk Stop");
