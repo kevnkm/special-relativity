8f85d867331a817116feac108de24608aa8a6ead
Kevin Kim <kk946@cornell.edu>
2025-07-27 13:33:21 +0900

    Add event scripts

---
+++ SpecialRelativity/Assets/Scripts/Event_12.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public class Event_12 : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log($"{gameObject.name} started.");
+        StartCoroutine(WaitBeforeNextNode());
+    }
+
+    private IEnumerator WaitBeforeNextNode()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("Transitioning to the next event.");
+        DialogueManager.Instance.StartNextNode();
+        Destroy(gameObject);
+    }
+}
