3a0f957b62f000230c964fc5646a01569faa7386
Kevin Kim <kk946@cornell.edu>
2025-07-27 17:43:27 +0900

    Update Event_11.cs

--- SpecialRelativity/Assets/Scripts/Event_11.cs
+++ SpecialRelativity/Assets/Scripts/Event_11.cs
@@ -13,6 +13,13 @@ public class Event_11 : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
 
+        DialogueManager.Instance.Train.transform.localScale = new Vector3(0.5f, 1, 1);
+        DialogueManager.Instance.Train.gameObject.SetActive(true);
+
+        yield return StartCoroutine(DialogueManager.Instance.MoveTrain(new Vector3(25, 0, 0), 3f));
+        yield return new WaitForSeconds(1f);
+        yield return StartCoroutine(DialogueManager.Instance.MoveTrain(new Vector3(-40, 0, 0), 8f));
+
         Debug.Log("Transitioning to the next event.");
         DialogueManager.Instance.StartNextNode();
         Destroy(gameObject);
