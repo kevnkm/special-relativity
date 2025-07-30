706ee1de3fee51c2d5648a220064a618ba05acf2
Kevin Kim <kk946@cornell.edu>
2025-07-30 20:06:39 +0900

    Update Event_0.cs

--- SpecialRelativity/Assets/Scripts/Event_0.cs
+++ SpecialRelativity/Assets/Scripts/Event_0.cs
@@ -5,11 +5,12 @@ public class Event_0 : MonoBehaviour
 {
     private void Start()
     {
-        StartCoroutine(Walk());
+        StartCoroutine(EventCoroutine());
     }
 
-    private IEnumerator Walk()
+    private IEnumerator EventCoroutine()
     {
+        StartCoroutine(DialogueManager.Instance.MoveTrainSmooth(new Vector3(-20f, 0f, 0f), 3f));
         yield return new WaitForSeconds(3f);
         DialogueManager.Instance.EinsteinAnimator.SetTrigger("Walk Start");
         yield return new WaitForSeconds(3f);
